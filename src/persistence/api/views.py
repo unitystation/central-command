55984f77d75b8a2c40ab6f90d47888e308690907
Andrés Franco Riquelme Sandoval <andresfranco.rs@gmail.com>
2020-11-07 22:02:42 -0300

    unstupidizing poly api random phrase

--- src/persistence/api/views.py
+++ src/persistence/api/views.py
@@ -1,10 +1,9 @@
-from django.db.models import Max
 from rest_framework import status
-from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from persistence.models import PolyPhrase
+from rest_framework.response import Response
+
 from persistence.api.serializers import PolyPhrasesSerializer
-import random
+from persistence.models import PolyPhrase
 
 
 @api_view(['GET'])
@@ -21,11 +20,7 @@ def poly_phrase_by_id_view(request, phrase_id):
 @api_view(['GET'])
 def poly_random_phrase_view(request):
     try:
-        max_id = PolyPhrase.objects.all().aggregate(max_id=Max("phrase_id"))['max_id']
-        if not max_id:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-        pk = random.randint(1, max_id)
-        phrase = PolyPhrase.objects.get(pk=pk)
+        phrase = PolyPhrase.objects.order_by('?').first()
     except Exception as e:
         error = {'error': f'{e}'}
         return Response(error, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
