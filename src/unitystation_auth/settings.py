a3260024819b7319b0df72c320a56e9aae0825e3
Andrés Franco Riquelme Sandoval <andresfranco.rs@gmail.com>
2020-11-15 17:04:58 -0300

    feat(api): set default api views to authenticated only

--- src/unitystation_auth/settings.py
+++ src/unitystation_auth/settings.py
@@ -96,6 +96,7 @@ DATABASES = {
 REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 10,
+    "DEFAULT_PERMISSION_CLASSES": ["rest_framework.permissions.IsAuthenticated"],
 }
 
 # Email settings
