dd13b020f49000097b869c6129beae18f688025d
Eugene <fogaprod@gmail.com>
2020-11-23 10:47:26 +0300

    refactor: switch postgres to trust mode eliminating env vars

--- src/unitystation_auth/settings.py
+++ src/unitystation_auth/settings.py
@@ -85,11 +85,11 @@ WSGI_APPLICATION = "unitystation_auth.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.environ["POSTGRES_DB"],
-        "USER": os.environ["POSTGRES_USER"],
-        "PASSWORD": os.environ["POSTGRES_PASSWORD"],
-        "HOST": os.environ["POSTGRES_HOST"],
-        "PORT": os.environ["POSTGRES_PORT"],
+        "NAME": os.environ.get("POSTGRES_DB", "postgres"),
+        "USER": os.environ.get("POSTGRES_USER", "postgres"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD", ""),
+        "HOST": os.environ.get("POSTGRES_HOST", "db"),
+        "PORT": os.environ.get("POSTGRES_PORT", ""),
     }
 }
 
