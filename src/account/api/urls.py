acbe3d6de30be91b7b70640ee79dc8e20bc80ac1
Andrés Riquelme <andresfranco.rs@gmail.com>
2020-12-28 03:22:59 -0300

    feat(account): endpoint for token generation
    Also included: wip for token authentication

--- src/account/api/urls.py
+++ src/account/api/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from rest_framework.authtoken import views
 
 from account.api.views import (
     register_account_view,
@@ -12,4 +13,5 @@ urlpatterns = [
     path("register/", register_account_view, name="Register"),
     path("get_account/", account_by_identifiers_view, name="Get Account"),
     path("get_character", character_by_identifier_view, name="Get Character"),
+    path("generate_token", views.obtain_auth_token, name="Generate Token"),
 ]
