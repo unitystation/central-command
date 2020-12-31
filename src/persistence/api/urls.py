201529cacf4fcc75f35dbbae1717fc84438eefb1
Andrés Franco Riquelme Sandoval <andresfranco.rs@gmail.com>
2020-12-31 20:37:35 -0300

    refactor(api): cleaned up poly related api urls

--- src/persistence/api/urls.py
+++ src/persistence/api/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 
-from persistence.api.views import poly_phrase_by_id_view, poly_random_phrase_view
+from persistence.api.views import (
+    poly_phrase_by_id_view,
+    poly_store_phrase_view,
+    poly_random_phrase_view,
+)
 
 app_name = "persistence"
 
 urlpatterns = [
-    path("", poly_random_phrase_view, name="Poly says"),
-    path("<phrase_id>/", poly_phrase_by_id_view, name="Phrase by id"),
+    path("polysays/", poly_random_phrase_view, name="Poly says"),
+    path("polysays/<phrase_id>/", poly_phrase_by_id_view, name="Phrase by id"),
+    path("post/polyphrase", poly_store_phrase_view, name="Post poly phrase"),
 ]
