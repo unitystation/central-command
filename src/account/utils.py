ccd0d1bdbb790da338db472663258efd9a01635e
Eugene <fogaprod@gmail.com>
2021-01-08 23:35:19 +0300

    refactor(account): refactor PushID and fix invocations, drop numpy (#32)
    Make next_id classmethod and store state in class because before it was reset on each invocation
    Remove all magic numbers
    Use snake_case names
    More small improvements like using reversed instead of negative ranges

--- src/account/utils.py
+++ src/account/utils.py
@@ -1,57 +1,70 @@
+import math
 import time
 import random
-
-import numpy
 
 
 class PushID:
     # Modeled after base64 web-safe chars, but ordered by ASCII.
     PUSH_CHARS = "-0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz"
 
-    def __init__(self):
+    # Lengths of resulting UID and it's parts
+    UID_CHARS = 20
 
-        # Timestamp of last push, used to prevent local collisions if you
-        # pushtwice in one ms.
-        self.lastPushTime = 0
+    UID_TIMESTAMP_CHARS = 8
+    UID_RANDOM_CHARS = UID_CHARS - UID_TIMESTAMP_CHARS
 
-        # We generate 72-bits of randomness which get turned into 12
-        # characters and appended to the timestamp to prevent
-        # collisions with other clients.  We store the last characters
-        # we generated because in the event of a collision, we'll use
-        # those same characters except "incremented" by one.
-        self.lastRandChars = numpy.empty(12, dtype=int)
+    # Shortcut
+    _push_chars_len = len(PUSH_CHARS)
 
-    def next_id(self):
-        now = int(time.time() * 1000)
-        duplicateTime = now == self.lastPushTime
-        self.lastPushTime = now
-        timeStampChars = numpy.empty(8, dtype=str)
+    # Timestamp of last push, used to prevent local collisions if you
+    # push multiple times in one ms.
+    _last_push_time = 0
 
-        for i in range(7, -1, -1):
-            timeStampChars[i] = self.PUSH_CHARS[now % 64]
-            now = int(now / 64)
+    # We generate 72-bits of randomness which get turned into 12
+    # characters and appended to the timestamp to prevent
+    # collisions with other clients.  We store the last characters
+    # we generated because in the event of a collision, we'll use
+    # those same characters except "incremented" by one.
+    _last_rand_chars = [0] * UID_RANDOM_CHARS
 
-        if now != 0:
-            raise ValueError("We should have converted the entire timestamp.")
+    @classmethod
+    def next_id(cls):
+        now = math.floor(time.time() * 1000)
+        same_millisecond = now == cls._last_push_time
 
-        uid = "".join(timeStampChars)
+        cls._last_push_time = now
 
-        if not duplicateTime:
-            for i in range(12):
-                self.lastRandChars[i] = int(random.random() * 64)
-        else:
+        timestamp_chars = [0] * cls.UID_TIMESTAMP_CHARS
+
+        for i in reversed(range(cls.UID_TIMESTAMP_CHARS)):
+            timestamp_chars[i] = cls.PUSH_CHARS[now % cls._push_chars_len]
+            now = math.floor(now / cls._push_chars_len)
+
+        assert now == 0, "We should have converted the entire timestamp"
+
+        uid = "".join(timestamp_chars)
+
+        if same_millisecond:
             # If the timestamp hasn't changed since last push, use the
-            # same random number, except incremented by 1.
-            for i in range(11, -1, -1):
-                if self.lastRandChars[i] == 63:
-                    self.lastRandChars[i] = 0
+            # same random number, except we increment it.
+            for i in reversed(range(cls.UID_RANDOM_CHARS)):
+                if cls._last_rand_chars[i] == cls._push_chars_len - 1:
+                    cls._last_rand_chars[i] = 0
                 else:
                     break
-            self.lastRandChars[i] += 1
 
-        for i in range(12):
-            uid += self.PUSH_CHARS[self.lastRandChars[i]]
+            cls._last_rand_chars[i] += 1
+        else:
+            for i in range(cls.UID_RANDOM_CHARS):
+                cls._last_rand_chars[i] = random.randint(0, cls._push_chars_len - 1)
 
-        if len(uid) != 20:
-            raise ValueError("Length should be 20.")
+        for i in range(cls.UID_RANDOM_CHARS):
+            uid += cls.PUSH_CHARS[cls._last_rand_chars[i]]
+
+        assert (
+            len(uid) == cls.UID_CHARS
+        ), f"UID length should be {0}, got {1}: {3}".format(
+            cls.UID_CHARS, len(uid), uid
+        )
+
         return uid
