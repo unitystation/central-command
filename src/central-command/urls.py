b6d92df58300a9a9effd0f971e18c77daa6ac330
Andrés Riquelme <andresfranco.rs@gmail.com>
2021-08-23 20:59:23 -0400

    misc(repo): rebrand the project as central-command

--- src/central-command/urls.py
+++ src/central-command/urls.py
@@ -1,4 +1,4 @@
-"""unitystation_auth URL Configuration
+"""central-command URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
