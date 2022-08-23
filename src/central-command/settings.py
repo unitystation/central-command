62396faf58eb5d3ddf534d94d489cc2ef1c3126f
Andrés Franco Riquelme Sandoval <andresfranco.rs@gmail.com>
2022-08-23 18:29:14 -0400

    refactor: drop need for postgres in unit tests

--- src/central-command/settings.py
+++ src/central-command/settings.py
@@ -86,12 +86,12 @@ WSGI_APPLICATION = "central-command.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.environ.get("DB_NAME", BASE_DIR / "db.sqlite3"),
+        "ENGINE": os.environ.get("DB_ENGINE", "django.db.backends.sqlite3"),
+        "NAME": os.environ.get("DB_NAME", str(BASE_DIR / "db.sqlite3")),
         "USER": os.environ.get("DB_USER", "postgres"),
         "PASSWORD": os.environ.get("DB_PASSWORD", ""),
         "HOST": os.environ.get("DB_HOST", "localhost"),
-        "PORT": os.environ.get("DB_PORT", ""),
+        "PORT": os.environ.get("DB_PORT", "5432"),
     }
 }
 
