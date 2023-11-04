052ae14a67cef1d86e238b695df1c91ea3bcf448
Gilles <43683714+corp-0@users.noreply.github.com>
2023-11-04 14:03:34 -0300

    fix: regex for usernames not allowing some valid usernames

--- src/accounts/validators.py
+++ src/accounts/validators.py
@@ -1,12 +1,15 @@
 from django.contrib.auth.validators import ASCIIUsernameValidator
 from django.utils.deconstruct import deconstructible
 
+IDENTIFIER_REGEX = r"^[a-zA-Z0-9_\-\.]{3,}$"
+USERNAME_REGEX = r"^[a-zA-Z0-9\.\-_](?!.* {2})[ \w.-]{1,}[a-zA-Z0-9\.\-_]$"
+
 
 @deconstructible()
 class AccountNameValidator(ASCIIUsernameValidator):
     # match account identifier that only has letters, numbers, dash, underscore
     # and at least 3 characters
-    regex = r"^[a-zA-Z0-9_\-]{3,}$"
+    regex = IDENTIFIER_REGEX
     message = (
         "Enter a valid account identifier. This value may contain only English letters, numbers, and -/_ characters."
     )
@@ -16,7 +19,7 @@ class AccountNameValidator(ASCIIUsernameValidator):
 class UsernameValidator(ASCIIUsernameValidator):
     # match username that has at least 3 characters, letters, numbers, dashes,
     # underscores, dots, and spaces but no consecutive whitespaces
-    regex = r"^[a-zA-Z0-9\.\-_](?!.* {2})[ \w.-_]{2,}$"
+    regex = USERNAME_REGEX
     message = (
         "Enter a valid username. This value may contain only English letters, "
         "numbers, dashes, underscores, dots, and spaces."
