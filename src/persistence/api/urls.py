233f132f7084822d3264dfee2d4fc8a3ba3d7eef
Gilles <43683714+corp-0@users.noreply.github.com>
2023-11-11 19:47:23 -0300

    feat: endpoints

--- src/persistence/api/urls.py
+++ src/persistence/api/urls.py
@@ -17,11 +17,11 @@ app_name = "persistence"
 
 urlpatterns = [
     path("characters", GetAllCharactersByAccountView.as_view(), name="characters-all"),
+    path("characters/create", CreateCharacterView.as_view(), name="characters-create"),
     path("characters/<int:pk>", GetCharacterByIdView.as_view(), name="characters-by-id"),
     path("characters/compatible", GetCompatibleCharacters.as_view(), name="characters-compatible"),
-    path("characters/create", CreateCharacterView.as_view(), name="characters-create"),
-    path("characters/update/<int:pk>", UpdateCharacterView.as_view(), name="characters-patch"),
-    path("characters/delete/<int:pk>", DeleteCharacterView.as_view(), name="characters-delete"),
+    path("characters/<int:pk>/update", UpdateCharacterView.as_view(), name="characters-patch"),
+    path("characters/<int:pk>/delete", DeleteCharacterView.as_view(), name="characters-delete"),
     path("other-data/read", ReadOtherDataView.as_view(), name="read"),
     path("other-data/write", WriteOtherDataView.as_view(), name="write"),
     path("poly-says", RandomPolyPhraseView.as_view(), name="poly-says"),
