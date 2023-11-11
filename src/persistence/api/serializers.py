094ec4dd07ca76c8de5c29ef654733c4227e0dc2
Gilles <43683714+corp-0@users.noreply.github.com>
2023-11-11 19:26:56 -0300

    feat: new characters endpoints

--- src/persistence/api/serializers.py
+++ src/persistence/api/serializers.py
@@ -1,6 +1,39 @@
 from rest_framework import serializers
 
-from ..models import Other, PolyPhrase
+from ..models import Character, Other, PolyPhrase
+
+
+class CompatibleCharactersRequestSerializer(serializers.Serializer):
+    fork_compatibility = serializers.CharField(max_length=25)
+    character_sheet_version = serializers.CharField(max_length=10)
+
+
+class CharacterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Character
+        fields = (
+            "id",
+            "account",
+            "fork_compatibility",
+            "character_sheet_version",
+            "data",
+        )
+
+        read_only_fields = ("id",)
+
+
+class UpdateCharacterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Character
+        fields = (
+            "id",
+            "account",
+            "fork_compatibility",
+            "character_sheet_version",
+            "data",
+        )
+
+        read_only_fields = ("id", "account")
 
 
 class OtherSerializer(serializers.ModelSerializer):
--- src/persistence/api/views.py
+++ src/persistence/api/views.py
@@ -1,11 +1,145 @@
 from django.core.exceptions import ObjectDoesNotExist, PermissionDenied
 from rest_framework import permissions, status
 from rest_framework.exceptions import ValidationError
-from rest_framework.generics import GenericAPIView
+from rest_framework.generics import GenericAPIView, ListAPIView
 from rest_framework.response import Response
 
-from ..models import Other, PolyPhrase
-from .serializers import OtherSerializer, PolyPhraseSerializer
+from ..models import Character, Other, PolyPhrase
+from .serializers import (
+    CharacterSerializer,
+    CompatibleCharactersRequestSerializer,
+    OtherSerializer,
+    PolyPhraseSerializer,
+    UpdateCharacterSerializer,
+)
+
+
+class GetCharacterByIdView(GenericAPIView):
+    serializer_class = CharacterSerializer
+
+    def get_queryset(self):
+        return Character.objects.filter(account__unique_identifier=self.request.user.unique_identifier)
+
+    def get(self, request, pk):
+        try:
+            character = Character.objects.get(pk=pk)
+        except ObjectDoesNotExist:
+            data = {"error": "No character with this ID could be found!"}
+            return Response(data, status=status.HTTP_404_NOT_FOUND)
+        except PermissionDenied:
+            data = {"error": "You do not have permission to view this character!"}
+            return Response(data, status=status.HTTP_403_FORBIDDEN)
+        serializer = self.serializer_class(character)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class GetCompatibleCharacters(ListAPIView):
+    serializer_class = CharacterSerializer
+
+    def get_queryset(self):
+        """
+        Retrieves a list of compatible characters.
+
+        Query Parameters:
+        - fork_compatibility: The fork compatibility string.
+        - character_sheet_version: The version string of the character sheet.
+
+        Example usage:
+        /api/characters/fork_compatibility=Unitystation&character_sheet_version=1.0.0
+        """
+        query_serializer = CompatibleCharactersRequestSerializer(data=self.request.query_params)
+        if not query_serializer.is_valid():
+            raise ValidationError(query_serializer.errors)
+
+        fork_compatibility = query_serializer.validated_data["fork_compatibility"]
+        character_sheet_version = query_serializer.validated_data["character_sheet_version"]
+
+        queryset = Character.objects.filter(
+            account__unique_identifier=self.request.user.pk,
+            fork_compatibility=fork_compatibility,
+            character_sheet_version=character_sheet_version,
+        )
+
+        return queryset
+
+
+class GetAllCharactersByAccountView(ListAPIView):
+    serializer_class = CharacterSerializer
+
+    def get_queryset(self):
+        """
+        Retrieves a list of all characters of an account.
+        """
+        unique_identifier = self.request.user.pk
+        queryset = Character.objects.filter(account__unique_identifier=unique_identifier)
+        return queryset
+
+
+class UpdateCharacterView(GenericAPIView):
+    serializer_class = UpdateCharacterSerializer
+    queryset = Character.objects.all()
+
+    def patch(self, request, pk):
+        try:
+            character = Character.objects.get(pk=pk)
+            if character.account != request.user:
+                raise PermissionDenied
+
+        except ObjectDoesNotExist:
+            data = {"error": "No character with this ID could be found!"}
+            return Response(data, status=status.HTTP_404_NOT_FOUND)
+        except PermissionDenied:
+            data = {"error": "You do not have permission to edit this character!"}
+            return Response(data, status=status.HTTP_403_FORBIDDEN)
+
+        serializer = self.get_serializer(character, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class DeleteCharacterView(GenericAPIView):
+    serializer_class = CharacterSerializer
+
+    def delete(self, request, pk):
+        try:
+            character = Character.objects.get(pk=pk)
+            if character.account != request.user:
+                raise PermissionDenied
+
+        except ObjectDoesNotExist:
+            data = {"error": "No character with this ID could be found!"}
+            return Response(data, status=status.HTTP_404_NOT_FOUND)
+        except PermissionDenied:
+            data = {"error": "You do not have permission to delete this character!"}
+            return Response(data, status=status.HTTP_403_FORBIDDEN)
+
+        character.delete()
+        data = {"success": "Character deleted successfully!"}
+        return Response(data, status=status.HTTP_200_OK)
+
+
+class CreateCharacterView(GenericAPIView):
+    serializer_class = CharacterSerializer
+
+    def post(self, request):
+        data_with_account = request.data.copy()
+        data_with_account["account"] = request.user.pk
+
+        serializer = self.serializer_class(data=data_with_account)
+        serializer.account = request.user
+        try:
+            serializer.is_valid(raise_exception=True)
+        except ValidationError as e:
+            data = {"error": str(e)}
+            return Response(data, status=status.HTTP_400_BAD_REQUEST)
+        except PermissionDenied:
+            data = {"error": "You do not have permission to write this data!"}
+            return Response(data, status=status.HTTP_403_FORBIDDEN)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
 class ReadOtherDataView(GenericAPIView):
