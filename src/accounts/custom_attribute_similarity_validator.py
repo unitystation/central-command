a5d1af82c45fdc2000b4d55a82e5b7447aa6b226
Gilles <43683714+corp-0@users.noreply.github.com>
2024-02-18 18:50:21 -0300

    chore: review suggestions

--- src/accounts/custom_attribute_similarity_validator.py
+++ src/accounts/custom_attribute_similarity_validator.py
@@ -1,19 +1,34 @@
 from django.contrib.auth.password_validation import UserAttributeSimilarityValidator
 from django.core.exceptions import ValidationError
+from django.db.models import Model
 
 from accounts.models import Account
 
 
 class CustomUserAttributeSimilarityValidator(UserAttributeSimilarityValidator):
-    def validate(self, password: str, user: Account | None = None):  # type: ignore
-        if not user:
+    def validate(self, password: str, user: Model | None = None):
+        if user is None:
             return
 
-        custom_attributes = [user.username, user.email, getattr(user, "unique_identifier", "")]
+        if not isinstance(user, Account):
+            raise Exception(
+                f"User is not an instance of Account: {user}. Check if the right user model is being set at settings.py."
+            )
+
+        custom_attributes = [
+            user.username,
+            user.email,
+            user.unique_identifier,
+        ]
+
         for attribute in custom_attributes:
             if not attribute or not password:
                 continue
-            if password.lower() in attribute.lower() or attribute.lower() in password.lower():
+
+            password = password.lower()
+            attribute = attribute.lower()
+
+            if password in attribute or attribute in password:
                 raise ValidationError(
                     "The password is too similar to the %(verbose_name)s.",
                     code="password_too_similar",
