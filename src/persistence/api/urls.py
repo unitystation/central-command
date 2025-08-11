d4420406613d27c3cb7871f8eb9cfbc347983d7c
craftxbox <craftxbox@craftxbox.com>
2025-08-10 23:33:06 -0230

    remove extraneous api paths & created migration

--- src/persistence/api/urls.py
+++ src/persistence/api/urls.py
@@ -2,16 +2,11 @@ from django.urls import path
 
 from .views import (
     CreateCharacterView,
-    CreateCharacterViewToken,
     DeleteCharacterView,
-    DeleteCharacterViewToken,
-    GenerateForkTokenView,
     GetAllCharactersByAccountView,
     GetCharacterByIdView,
     GetCompatibleCharacters,
-    GetCompatibleCharactersToken,
     UpdateCharacterView,
-    UpdateCharacterViewToken,
 )
 
 app_name = "persistence"
@@ -23,17 +18,4 @@ urlpatterns = [
     path("characters/compatible", GetCompatibleCharacters.as_view(), name="characters-compatible"),
     path("characters/<int:pk>/update", UpdateCharacterView.as_view(), name="characters-patch"),
     path("characters/<int:pk>/delete", DeleteCharacterView.as_view(), name="characters-delete"),
-    path(
-        "characters/<int:pk>/updateToken", UpdateCharacterViewToken.as_view(), name="characters-patch-token"
-    ),  # PutAccountsCharacterByIDByCharactersToken
-    path(
-        "characters/createToken", CreateCharacterViewToken.as_view(), name="characters-create-token"
-    ),  # PostMakeAccountsCharacterByCharactersToken
-    path(
-        "characters/compatibleToken", GetCompatibleCharactersToken.as_view(), name="characters-compatible-token"
-    ),  # GetCharactersByCharacterSheetToken
-    path(
-        "characters/<int:pk>/deleteToken", DeleteCharacterViewToken.as_view(), name="characters-delete-token"
-    ),  # DeleteAccountsCharacterByIDByCharactersToken
-    path("characters/GenForkToken", GenerateForkTokenView.as_view(), name="Gen-Fork-token"),
 ]
