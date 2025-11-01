903c5a409e23b2576d01e28c91a7192f258e5b88
Gilles <43683714+corp-0@users.noreply.github.com>
2025-11-01 02:05:50 -0300

    feat: ported current serverlist logic to cc

--- src/central_command/settings.py
+++ src/central_command/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     "post_office",
     "accounts",
     "persistence",
+    "baby_serverlist",
     "drf_spectacular",
 ]
 
@@ -151,6 +152,16 @@ DATABASES = {
         "PASSWORD": os.environ.get("DB_PASSWORD", ""),
         "HOST": os.environ.get("DB_HOST", "localhost"),
         "PORT": os.environ.get("DB_PORT", "5432"),
+    }
+}
+
+MEMCACHED_HOST = os.environ.get("MEMCACHED_HOST", "cache")
+MEMCACHED_PORT = os.environ.get("MEMCACHED_PORT", "11211")
+
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.memcached.PyMemcacheCache",
+        "LOCATION": f"{MEMCACHED_HOST}:{MEMCACHED_PORT}",
     }
 }
 
