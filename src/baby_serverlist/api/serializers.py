0fa92e30608d087f930ad77751cf36b1c534a30f
Gilles <43683714+corp-0@users.noreply.github.com>
2025-11-02 20:18:38 -0300

    feat: annotate endpoint responses for auto documentation

--- src/baby_serverlist/api/serializers.py
+++ src/baby_serverlist/api/serializers.py
@@ -22,9 +22,41 @@ class ServerStatusSerializer(serializers.Serializer):
     GoodFileVersion = serializers.CharField()
 
 
-class ActiveServersSerializer(serializers.Serializer):
-    CashDateTime = serializers.DateTimeField()
-    servers = ServerStatusSerializer(many=True)
+class CachedServerStatusSerializer(serializers.Serializer):
+    Passworded = serializers.BooleanField()
+    ServerName = serializers.CharField()
+    ForkName = serializers.CharField()
+    BuildVersion = serializers.IntegerField()
+    CurrentMap = serializers.CharField()
+    GameMode = serializers.CharField()
+    IngameTime = serializers.CharField()
+    RoundTime = serializers.CharField()
+    PlayerCount = serializers.IntegerField()
+    PlayerCountMax = serializers.IntegerField()
+    ServerIP = serializers.CharField()
+    ServerPort = serializers.IntegerField()
+    WinDownload = serializers.CharField()
+    OSXDownload = serializers.CharField()
+    LinuxDownload = serializers.CharField()
+    fps = serializers.IntegerField()
+    GoodFileVersion = serializers.CharField()
+
+
+class OwnedBabyServerSerializer(serializers.Serializer):
+    id = serializers.UUIDField()
+    whitelisted = serializers.BooleanField()
+    live = serializers.BooleanField()
+    status = CachedServerStatusSerializer(allow_null=True)
+
+
+class BabyServerTokenSerializer(serializers.Serializer):
+    id = serializers.UUIDField()
+    serverlist_token = serializers.CharField()
+    whitelisted = serializers.BooleanField()
+
+
+class BabyServerStatusListSerializer(serializers.Serializer):
+    servers = CachedServerStatusSerializer(many=True)
 
 
 class RegenerateServerlistTokenSerializer(serializers.Serializer):
